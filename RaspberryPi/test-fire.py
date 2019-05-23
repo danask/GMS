7d8d22f359b0aa493bbdf999cd38b61d05714deb
Daniel_Admin <danask2008@gmail.com>
2019-05-22 22:34:34 -0700

    Update camera and motion sensor

--- RaspberryPi/test-fire.py
+++ RaspberryPi/test-fire.py
@@ -6,6 +6,11 @@ from firebase_admin import credentials
 from firebase_admin import db
 from threading import Thread
 
+from picamera import PiCamera
+from gpiozero import MotionSensor
+from time import sleep
+
+
 import RPi.GPIO as GPIO
 import dht11
 import time
@@ -14,7 +19,11 @@ import datetime
 # initialize GPIO
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
-GPIO.cleanup()
+GPIO.setup(20, GPIO.IN)
+
+#GPIO.cleanup()
+        
+
 
 
 
@@ -53,7 +62,9 @@ class TEST():
         
         # read data using pin 21
         instance = dht11.DHT11(pin=21)
-
+        pir = MotionSensor(20)        
+        camera = PiCamera()
+            
         while True:
             result = instance.read()
             if result.is_valid():
@@ -64,7 +75,31 @@ class TEST():
                 self.msg.set(str(datetime.datetime.now()))
                 self.temp.set(result.temperature)
                 self.humid.set(result.humidity)
-                
+        
+            #if GPIO.input(20):
+            #    print("Motion detected...")
+
+            state = GPIO.input(20)
+            
+            
+            if state==0:
+                print "nothing..."
+                time.sleep(0.1)
+            elif state==1:
+                print "something here..."
+                camera.capture('/home/pi/python-fire/Pics/imagetest.jpg')
+
+            #pir.wait_for_motion
+            #print("Alert")
+            
+            #camera.start_preview()
+
+            #for i in range (0, 5):
+             #   sleep(5)
+            #camera.capture('/home/pi/python-fire/Pics/imagetest.jpg')
+            #camera.stop_preview()
+            
+                    
             time.sleep(1)
         
         print('GET Okay!!')
@@ -81,3 +116,4 @@ test = TEST()
 #msg = Thread(target= test.setDhtDB)
 #msg.daemon = True
 #msg.start()
+
