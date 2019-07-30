9164a3e5663e76e5150f1dc96af2c9b920f6ac9b
Daniel <danask2008@gmail.com>
2019-07-29 21:38:24 -0700

    Update regression logic and chart

--- Recommendation/GMS_TF_ML.py
+++ Recommendation/GMS_TF_ML.py
@@ -10,8 +10,9 @@ import mysql.connector
 while True:
 
     # init values
-    x1_input = 25.0;
-    x2_input = 50.0;
+    isExist = 0
+    x1_input = 25.0
+    x2_input = 50.0
 
     mydb = mysql.connector.connect(
         host = "localhost",
@@ -23,19 +24,22 @@ while True:
     mycursor = mydb.cursor()
 
     mycursor.execute("SELECT sensorTemp, sensorHumid FROM sensor "+
-                     "where description = 'demo' or description = null")
+                     "where description = 'demo' or description = ''")
     myresult = mycursor.fetchall()
 
     for rows in myresult:
         x1_input = float(rows[0])
         x2_input = float(rows[1])
-        print(x1_input)
+        
+        if x1_input > 0:
+            print(x1_input)
+            isExist = 1
 
 
 
     # data
-    x1_data = [21., 22., 23., 24., 25., 26., 27., 28., 29., 30.] # room_temp
-    x2_data = [70., 65., 60., 55., 50., 45., 40., 35., 30., 25.] # room_humidity
+    x1_data = [21., 22., 23., 24., 25., 26., 27., 28., 29., 30.] # room_temp_criteria
+    x2_data = [70., 65., 60., 55., 50., 45., 40., 35., 30., 25.] # room_humidity_criteria
     #x3_data = [75., 93., 90., 100., 70.]
     y_data = [1.6, 1.7, 1.8, 1.9, 2.0, 2.1, 2.2, 2.3, 2.4, 2.5]
      
@@ -60,21 +64,23 @@ while True:
     train = optimizer.minimize(cost)
      
     # Session
-    with tf.Session() as sess:
-        
-        sess.run(tf.global_variables_initializer())
-     
-        for step in range(5001):
-            cost_val, hy_val, _ = sess.run([cost, hypothesis, train],
-                                           feed_dict={x1: x1_data, x2: x2_data, Y: y_data})
-                                           #feed_dict={x1: x1_data, x2: x2_data, x3: x3_data, Y: y_data})
-            #if step % 500 == 0:
-            #    print(step, "Cost: ", cost_val, "\nPrediction:\n", hy_val)
-        result = sess.run(hypothesis, feed_dict={x1: [x1_input], x2: [x2_input]})
-        print(result)
+    if isExist > 0:
+        with tf.Session() as sess:
+            
+            sess.run(tf.global_variables_initializer())
+         
+            for step in range(5001):
+                cost_val, hy_val, _ = sess.run([cost, hypothesis, train],
+                                               feed_dict={x1: x1_data, x2: x2_data, Y: y_data})
+                                               #feed_dict={x1: x1_data, x2: x2_data, x3: x3_data, Y: y_data})
+                #if step % 500 == 0:
+                #    print(step, "Cost: ", cost_val, "\nPrediction:\n", hy_val)
+            result = sess.run(hypothesis, feed_dict={x1: [x1_input], x2: [x2_input]})
+            print(result)
 
-        sql = "UPDATE sensor SET description = '" + str(result[0]) + "' WHERE description = 'demo' or description = null"
-        #print (sql)
-        mycursor.execute(sql)
-        mydb.commit()
-        print(mycursor.rowcount, "record(s) affected")
+            sql = "UPDATE sensor SET description = '" + str(result[0]) + "' WHERE description = 'demo' or description = ''"
+            #print (sql)
+            mycursor.execute(sql)
+            mydb.commit()
+            print(mycursor.rowcount, "record(s) affected")
+            isExist = 0
