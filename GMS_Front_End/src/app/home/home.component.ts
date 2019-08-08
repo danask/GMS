83c2a09efab9cd70c4b97132338ad6084a7c69fa
Daniel <danask2008@gmail.com>
2019-08-07 20:51:21 -0700

    Update the description

--- GMS_Front_End/src/app/home/home.component.ts
+++ GMS_Front_End/src/app/home/home.component.ts
@@ -10,6 +10,28 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.imageModal();
   }
+  public imageModal()
+  {
 
+    // Get the modal
+    var modal = document.getElementById("myModal");
+
+    // Get the image and insert it inside the modal - use its "alt" text as a caption
+    var img = document.getElementById("myImg") as HTMLImageElement;
+    var modalImg = document.getElementById("img01") as HTMLImageElement;
+    var captionText = document.getElementById("caption") as HTMLImageElement;
+
+    img.onclick = function(){
+      modal.style.display = "block";
+      modalImg.src = img.src;
+      captionText.innerHTML = img.alt;
+    }
+
+    var span = document.getElementsByClassName("close")[0] as HTMLImageElement;
+    span.onclick = function(){
+      modal.style.display = "none";
+    } 
+  }
 }
