48b33154b071f08f6422d241a680efdc12d9e1b7
Daniel <danask2008@gmail.com>
2019-08-27 15:32:38 -0700

    Update 2nd class

--- RaspberryPi/legacy/GmsDataCollector.py
+++ RaspberryPi/legacy/GmsDataCollector.py
@@ -183,54 +183,48 @@ class GmsDataCollector():
 
         #print("Done!") 
 
+def wateringMotor(num):
+    control_pins = [7,11,13,15]
+    for pin in control_pins:
+      GPIO.setup(pin, GPIO.OUT)
+      GPIO.output(pin, 0)
+      
+    halfstep_seq = [
+      [1,0,0,0],
+      [1,1,0,0],
+      [0,1,0,0],
+      [0,1,1,0],
+      [0,0,1,0],
+      [0,0,1,1],
+      [0,0,0,1],
+      [1,0,0,1]
+    ]
 
-class GmsWateringMotor():
+     # rotation
+    wateringDuration = 10
 
-    def __init__(self):
-
-
-# def wateringMotor(num):
-        control_pins = [7,11,13,15]
-        for pin in control_pins:
-          GPIO.setup(pin, GPIO.OUT)
-          GPIO.output(pin, 0)
-          
-        halfstep_seq = [
-          [1,0,0,0],
-          [1,1,0,0],
-          [0,1,0,0],
-          [0,1,1,0],
-          [0,0,1,0],
-          [0,0,1,1],
-          [0,0,0,1],
-          [1,0,0,1]
-        ]
-
-         # rotation
-        wateringDuration = 10
-
-        try:        
-            while True:
-                      
-                for times in range (wateringDuration):
-                    for i in range(256):
-                      for halfstep in range(8):
-                        for pin in range(4):
-                          GPIO.output(control_pins[pin], halfstep_seq[halfstep][pin])
-                        time.sleep(0.001)
-                        
-                wateringDuration = 0
-
-                # check and get the rotation number from Firebase
-                        
-                if wateringDuration == 0:
-                    print("Stand by: waiting for the watering input from GMS server...")
-                    time.sleep(10.0)
-                    #break;
+    try:        
+        while True:
+                  
+            for times in range (wateringDuration):
+                for i in range(256):
+                  for halfstep in range(8):
+                    for pin in range(4):
+                      GPIO.output(control_pins[pin], halfstep_seq[halfstep][pin])
+                    time.sleep(0.001)
                     
-        except KeyboardInterrupt:
-            GPIO.cleanup()
-            raise
+            wateringDuration = 0
+
+            # check and get the rotation number from Firebase
+                    
+            if wateringDuration == 0:
+                print("Stand by: waiting for the watering input from GMS server...")
+                time.sleep(10.0)
+                #break;
+                
+    except KeyboardInterrupt:
+        GPIO.cleanup()
+        raise
     
    
 def setDhtDB(self):
@@ -260,9 +254,7 @@ def print_square(num):
 #        GPIO.cleanup()
         
 # Start
-gmsWateringMotor = GmsWateringMotor()
 gmsDataCollector = GmsDataCollector()
-
 
 #msg = Thread(target= test.setDhtDB)
 #msg.daemon = True
