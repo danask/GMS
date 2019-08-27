b4dc4230f5b4c739586d67a4c14b6f580240d8a9
Daniel_Admin <danask2008@gmail.com>
2019-08-27 01:12:14 -0700

    Update the file

--- RaspberryPi/GmsDataCollector.py
+++ RaspberryPi/GmsDataCollector.py
@@ -12,10 +12,10 @@ import lcddriver
 from mfrc522 import SimpleMFRC522
 
 # time related library
+import time
+import datetime
 from time import sleep
-from datetime import datetime
-#import time
-#import datetime
+#from datetime import datetime
 
 # firebase info. to use database and storage
 import firebase_admin
@@ -24,7 +24,7 @@ from firebase_admin import db
 from firebase_admin import storage
 
 # multi-threading
-from threading import Thread
+import threading
 
 # initialize GPIO
 GPIO.setwarnings(False)
@@ -47,10 +47,11 @@ REF_ID = 'id'
 REF_TIME = 'detectTime'
 
 
-class GMSSensors():
+class GmsDataCollector():
 
     def __init__(self):
-        
+#def runSensors(num): 
+
         # firebase credential
         cred = credentials.Certificate(PATH_CRED)
 
@@ -58,7 +59,8 @@ class GMSSensors():
             'databaseURL': URL_DB,
             'storageBucket': URL_STR
         })
-
+        bucket = storage.bucket()
+        
         # get and test them
         self.msg = db.reference('timestamp')
         
@@ -80,7 +82,7 @@ class GMSSensors():
 
         # read data using pin 21(DHT11), 20 (PIR), LCD, Camera, RC522
         dht = dht11.DHT11(pin=21)
-        pir = GPIO.input(20) #MotionSensor(20)
+        #pir = MotionSensor(20) #GPIO.input(20) 
         lcd = lcddriver.lcd()
         camera = PiCamera()
         reader = SimpleMFRC522() # StepMotor
@@ -96,97 +98,163 @@ class GMSSensors():
         
         time.sleep(2) # to stabilize sensor
         
-        while True:
-            
-            # timestamp
-            currentTime = str(datetime.datetime.now())
-            
-            if dht.read().is_valid():
-
-                print("Last valid input: " + currentTime)
+        try:
+            while True:
                 
-                tempStr = "Temperature: "+  str(result.temperature) + "c"
-                humidStr = "Humidity: "+ str(result.humidity) +"%"
-                #print("Temperature: %d C" % result.temperature)
-                #print("Humidity: %d %%" % result.humidity)
-
-                print(tempStr)
-                print(humidStr)
-
-                print("Writing to display LCD")
-                lcd.lcd_clear()
-                lcd.lcd_display_string(tempStr, 1)
-                lcd.lcd_display_string(humidStr, 2)
+                # timestamp
+                currentTime = str(datetime.datetime.now())
+                tempHumid = dht.read()
                 
-                # send them to firebase
-                ##self.msg.set(currentTime)
-                ##self.temp.set(result.temperature)
-                ##self.humid.set(result.humidity)
-        
+                if tempHumid.is_valid():
 
-#            state = GPIO.input(20)
+                    print("**Last valid input: " + currentTime)
+                    
+                    tempStr = "Temperature: "+  str(tempHumid.temperature) + "c"
+                    humidStr = "Humidity: "+ str(tempHumid.humidity) +"%"
+                    #print("Temperature: %d C" % result.temperature)
+                    #print("Humidity: %d %%" % result.humidity)
+
+                    print(tempStr)
+                    print(humidStr)
+
+                    print("Writing to display LCD")
+                    lcd.lcd_clear()
+                    lcd.lcd_display_string(tempStr, 1)
+                    lcd.lcd_display_string(humidStr, 2)
+                    
+                    # send them to firebase
+                    ##self.msg.set(currentTime)
+                    ##self.temp.set(result.temperature)
+                    ##self.humid.set(result.humidity)
             
-            #db = firestore.client()
-            bucket = storage.bucket()
-            print bucket
-        
-            if pir == 0:
-                print ("nothing...")
-                #self.alert.set("false")
-                time.sleep(0.5)
 
-            elif pir == 1:
-                print ("something's here...")
-                lcd.lcd_clear()
-                lcd.lcd_display_string("[Alert]", 1)
-                lcd.lcd_display_string("Motion Detect", 2)
-                
+                pir = GPIO.input(20)
+            
+                if pir == 0:
+                    print ("nothing...")
+                    time.sleep(0.5)
 
-                # RFID (2019/08/25)
-                GPIO.output(26, True)
-                time.sleep(0.5)
-                                
-                print("Hold a tag near the reader")
-                id, text = reader.read()
-                print("ID: %s\nText: %s" % (id,text))
-                                
-                if id == 46054386582: # temp
-                    GPIO.output(26, False)
+                elif pir == 1:
+                    print ("something's here...")
                     lcd.lcd_clear()
                     lcd.lcd_display_string("[Alert]", 1)
-                    lcd.lcd_display_string("Clear by Admin", 2)
-                    time.sleep(3)
-                
-                # update the record to firebase database
-                detectTime = str(datetime.datetime.now())
-                ##self.time.set(detectTime)
-                ##self.alert.set("Motion Detect")
-
-                # store the image in local space temporarily
-                camera.capture('/home/pi/GMS/Pics/imagetest.jpg')
-                
-                blobName = 'CapturedImages/'+ detectTime + '.jpg'
-                blob = bucket.blob(blobName)
-
-                # upload the file to firebase storage
-                ##blob.upload_from_filename(filename='/home/pi/python-fire/Pics/imagetest.jpg')
-                ##self.id.set(blob.public_url) 
-
-                # update this to check
-                #print(blob.generate_signed_url(datetime.timedelta(seconds=300), method='GET')) 
-                ##self.id.set(blob.generate_signed_url(datetime.timedelta(seconds=300), method='GET'))
-                time.sleep(3) # to avoid multiple detections                
+                    lcd.lcd_display_string("Motion Detect", 2)
                     
-            time.sleep(2) # loop delay, should be less than detection delay
+
+                    # RFID (2019/08/25)
+                    GPIO.output(26, True)
+                    time.sleep(0.5)
+                                    
+                    print("Hold a tag near the reader")
+                    id, text = reader.read()
+                    print("ID: %s\nText: %s" % (id,text))
+                                    
+                    if id == 46054386582: # temp
+                        GPIO.output(26, False)
+                        lcd.lcd_clear()
+                        lcd.lcd_display_string("[Alert]", 1)
+                        lcd.lcd_display_string("Clear by Admin", 2)
+                        time.sleep(3)
+                    
+                    # update the record to firebase database
+                    detectTime = str(datetime.datetime.now())
+                    ##self.time.set(detectTime)
+                    ##self.alert.set("Motion Detect")
+
+                    # store the image in local space temporarily
+                    camera.capture('/home/pi/GMS/Pics/imagetest.jpg')
+                    
+                    blobName = 'CapturedImages/'+ detectTime + '.jpg'
+                    blob = bucket.blob(blobName)
+
+                    # upload the file to firebase storage
+                    ##blob.upload_from_filename(filename='/home/pi/python-fire/Pics/imagetest.jpg')
+                    ##self.id.set(blob.public_url) 
+
+                    # update this to check
+                    #print(blob.generate_signed_url(datetime.timedelta(seconds=300), method='GET')) 
+                    ##self.id.set(blob.generate_signed_url(datetime.timedelta(seconds=300), method='GET'))
+                    time.sleep(3) # to avoid multiple detections                
+                        
+                time.sleep(2) # loop delay, should be less than detection delay
+                
+        except KeyboardInterrupt:
+            GPIO.cleanup()
+
+        #print("Done!") 
+
+def wateringMotor(num):
+    control_pins = [7,11,13,15]
+    for pin in control_pins:
+      GPIO.setup(pin, GPIO.OUT)
+      GPIO.output(pin, 0)
+      
+    halfstep_seq = [
+      [1,0,0,0],
+      [1,1,0,0],
+      [0,1,0,0],
+      [0,1,1,0],
+      [0,0,1,0],
+      [0,0,1,1],
+      [0,0,0,1],
+      [1,0,0,1]
+    ]
+
+     # rotation
+    wateringDuration = 10
+
+    try:        
+        while True:
+                  
+            for times in range (wateringDuration):
+                for i in range(256):
+                  for halfstep in range(8):
+                    for pin in range(4):
+                      GPIO.output(control_pins[pin], halfstep_seq[halfstep][pin])
+                    time.sleep(0.001)
+                    
+            wateringDuration = 0
+
+            # check and get the rotation number from Firebase
+                    
+            if wateringDuration == 0:
+                print("Stand by: waiting for the watering input from GMS server...")
+                time.sleep(10.0)
+                #break;
+                
+    except KeyboardInterrupt:
+        GPIO.cleanup()
+        raise
+    
+   
+def setDhtDB(self):
+    self.msg.set('testsend')
+    print(self.msg.get())            
         
-    def setDhtDB(self):
-        self.msg.set('testsend')
-        print(self.msg.get())            
-        
-        
+
+def print_cube2(num): 
+    print("Cube: {}".format(num * num * num)) 
+
+def print_square(num): 
+    print("Square: {}".format(num * num)) 
+
+
+
+#def __init__(self):
+#if __name__ == "__main__":     
+
+ #   try:
+        #t1 = threading.Thread(target=runSensors, args=(10,)) 
+ #       t2 = threading.Thread(target=wateringMotor, args=(10,)) 
+      
+        #t1.start() 
+ #       t2.start() 
+    
+#    except KeyboardInterrupt:
+#        GPIO.cleanup()
         
 # Start
-test = GMSSensors()
+gmsDataCollector = GmsDataCollector()
 
 #msg = Thread(target= test.setDhtDB)
 #msg.daemon = True
